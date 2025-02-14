174c6a3adc74000f473d403283cc44c25741f6e0
Nil Kantha Gaudel <nirmalgaudel63@gmail.com>
2025-02-14 18:59:31 +0545

    added db ssl variable

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -9,7 +9,8 @@ class Database {
                 user: process.env.DB_USER,
                 password: process.env.DB_PASS,
                 database: process.env.DB_NAME,
-                port: process.env.DB_PORT || 3306
+                port: process.env.DB_PORT || 3306,
+                ssl: process.env.DB_SSL_MODE
             });
 
             this.connection.connect((err) => {
