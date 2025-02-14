3a1a6d6559e178e66c35672b236b04581a95693f
Nil Kantha Gaudel <nirmalgaudel63@gmail.com>
2025-02-14 18:57:08 +0545

    added db port variable

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -8,7 +8,8 @@ class Database {
                 host: process.env.DB_HOST,
                 user: process.env.DB_USER,
                 password: process.env.DB_PASS,
-                database: process.env.DB_NAME
+                database: process.env.DB_NAME,
+                port: process.env.DB_PORT || 3306
             });
 
             this.connection.connect((err) => {
